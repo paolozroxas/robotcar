a8ecbd09f779c43049595dc0e0d4b57d72d348fc
paolozroxas <paolo.z.roxas@gmail.com>
2019-09-26 20:21:20 -0700

    initial

---
+++ controls.js
@@ -0,0 +1,56 @@
+const keypress = require('keypress');
+keypress(process.stdin);
+
+const { Gpio } = require('onoff');
+const leftTrackForward = new Gpio(27, 'out');
+const leftTrackBackward = new Gpio(22, 'out');
+const rightTrackForward = new Gpio(4, 'out');
+const rightTrackBackward = new Gpio(17, 'out');
+
+
+process.stdin.on('keypress', (ch, key) => {
+  if (key && key.ctrl && key.name == 'c') {
+    process.stdin.pause();
+    return;
+  }
+
+  if (key) {
+    switch(key.name) {
+      case 'up':
+        moveForward();
+        break;
+      case 'left':
+        turnLeft();
+        break;
+      case 'right':
+        turnRight();
+        break;
+      case 'down':
+        moveBackward();
+        break;
+    }
+  }
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
+
+const moveForward = () => {
+  leftTrackForward.write(Gpio.HIGH);
+  rightTrackForward.write(Gpio.HIGH);
+}
+
+const turnRight = () => {
+  leftTrackForward.write(Gpio.HIGH);
+  rightTrackBackward.write(Gpio.HIGH);
+}
+
+const turnLeft = () => {
+  leftTrackBackward.write(Gpio.HIGH);
+  rightTrackForward.write(Gpio.HIGH);
+}
+
+const moveBackward = () => {
+  leftTrackBackward.write(Gpio.HIGH);
+  rightTrackBackward.write(Gpio.HIGH);
+}
