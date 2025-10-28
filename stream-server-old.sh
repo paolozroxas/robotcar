6700a557d8fbbea5e7586c595ba9540394ff400f
Paolo Z. Roxas <paolo.z.roxas@gmail.com>
2025-10-28 15:37:00 +0800

    vertical flip

--- stream-server-old.sh
+++ stream-server-old.sh
@@ -1,1 +1,1 @@
-raspivid -t 0 -w 1280 -h 720 -fps 25 -b 2000000 -o - | cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8080}' :demux=h264
+raspivid -t 0 -vf -w 1280 -h 720 -fps 25 -b 2000000 -o - | cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8080}' :demux=h264
