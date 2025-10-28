373a6c5a4f55b86e19f5f2cde19a10d069b2edb8
Paolo Z. Roxas <paolo.z.roxas@gmail.com>
2025-10-28 14:06:15 +0800

    add stream command script

---
+++ stream.sh
@@ -0,0 +1,1 @@
+raspivid -t 0 -h 720 -w 1080 -fps 25 -hf -vf -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h260parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=192.168.68.68 port=5000
